d5a016c309d61789a99e301ef50fc035ea805fc1
Stheven <stheven4496@gmail.com>
2023-07-18 16:42:10 +0700

    link fix

---
+++ build/assets/home-2dbfa8c1.js
@@ -0,0 +1,2 @@
+import{a as s,j as e}from"./index-6373ff9b.js";import{S as a}from"./sideBar-7076b631.js";import"./logo-62ff978e.js";const i=()=>{const t=s(o=>o.nama);return console.log(t),e.jsx(e.Fragment,{children:e.jsx(a,{content:e.jsxs("div",{className:"flex flex-col font-Poppins  justify-center items-center pt-12 gap-14",children:[e.jsx("span",{className:"text-2xl font-bold ",children:`"Learn as if you will live forever
+live like you will die tomorrow."`}),e.jsx("span",{className:"text-lg font-bold",children:"Mahatma Gandhi"})]})})})};export{i as default};
